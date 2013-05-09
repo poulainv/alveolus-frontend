9880fd25331525531f351d8b46f5b8db03cf8840
Agathe <qryyk@yahoo.fr>
2013-05-09 10:58:47 +0200

    add form comment, rating, dev view

--- app/js/controllers/webAppDescCtrl.js
+++ app/js/controllers/webAppDescCtrl.js
@@ -11,6 +11,12 @@ controller('WebAppDescCtrl', function($scope, $routeParams, Webapp, WebappFacebo
 	// $scope.facebook=WebappFacebook.get({id: $scope.facebook_id}); // Requête cross domain ne marche pas
 
 	$scope.user=User.get({id: 1});
-	$scope.categorie=Categorie.get({id: 2}); // $scope.result.webapp.category_id Ne marche pas !!
-	$scope.comments=WebappComments.get({id: $routeParams.webAppId}); // probleme retour résultat
+	$scope.category=Categorie.get({id: 2}); // $scope.result.webapp.category_id Ne marche pas !!
+	// $scope.comments=WebappComments.get({id: $routeParams.webAppId}); // probleme retour résultats ???
+	$scope.comments=[{"body":"Very interesting","created_at":"2013-05-07T18:14:25Z","id":5,"rating":3,"updated_at":"2013-05-07T18:14:25Z","user_id":2,"webapp_id":2},{"body":"En anglais, je comprends rien","created_at":"2013-05-07T18:14:25Z","id":4,"rating":4,"updated_at":"2013-05-07T18:14:25Z","user_id":1,"webapp_id":2}];
+
+	$scope.save = function() {
+    $scope.comment = angular.copy($scope.comment, $scope.master);
+    alert($scope.comment.rating);
+  };
 });

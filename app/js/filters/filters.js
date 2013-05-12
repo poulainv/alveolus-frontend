a6c3ec640afa7a6f0856ea8b8e66eb2c3da51565
Agathe <qryyk@yahoo.fr>
2013-05-12 22:54:40 +0200

    webapp2
    Je savais bien que j'avais oublié un truc

--- app/js/filters/filters.js
+++ app/js/filters/filters.js
@@ -4,7 +4,7 @@
 
 angular.module('alveolus.filters', []).filter('star', function() {
   return function(input) {
-    if(((input.rate-input.index)>0.33) && ((input.rate-input.index)<0.66)) return 'icon-star-half';
+    if(((input.rate-input.index)>0.33) && ((input.rate-input.index)<0.66)) return 'icon-star-half-empty';
     else if(input.rate>input.index) return 'icon-star';
     else return 'icon-star-empty';
   };

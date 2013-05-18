7fb5f903a2db9b037e25063a43a2e8af6b51f3b6
vpoulain <vincent.poulain2@gmail.com>
2013-05-18 13:48:59 +0200

    add Connexion modal

--- app/js/controllers/mainCtrl.js
+++ app/js/controllers/mainCtrl.js
@@ -3,9 +3,54 @@
 /* Controleur de la home page */
 
 angular.module('alveolus.mainCtrl', []).
-controller('MainCtrl', function($scope,$routeParams,WebappService) {
+controller('MainCtrl', function($scope,$routeParams,WebappService, SessionService) {
+
+	var alertLogSuccess = { type: 'success', msg: 'Parfait, vous êtes correctement authentifié' } ;
+	var alertLogFail = { type: 'error', msg: 'Oops, impossible de vous authentifié' } ;
+
+	$scope.alerts = [];
+
+	$scope.user = SessionService.getUser();
 
 	$scope.search = function(content){
 		WebappService.setSearchContent(content);
-	}
+	};
+
+	$scope.sign_in = function(user){
+		console.log($scope.user.email);
+		SessionService.sign_in($scope.user,function(user){
+			if(user.authorized){
+				$scope.user = user ;
+				addAlert(alertLogSuccess);
+			}else{
+				$scope.user = user ;
+				addAlert(alertLogFail);
+			}
+		});
+	};
+
+
+	var addAlert = function(alert) {
+		$scope.alerts.push(alert);
+	};
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
+
+	$scope.openModalLogin = function () {
+		$scope.shouldBeOpen = true;
+	};
+
+	$scope.close = function () {
+		$scope.closeMsg = 'I was closed at: ' + new Date();
+		$scope.shouldBeOpen = false;
+	};
+
+	$scope.opts = {
+		backdropFade: true,
+		dialogFade:true
+	};
+
 });

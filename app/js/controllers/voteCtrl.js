9448028e3c5bb153dc35e2812dfdb553ec421c03
Johan Soulet <johan.soulet@gmail.com>
2013-05-21 15:44:53 +0200

    Test login on vote load + send voting request and update the page

--- app/js/controllers/voteCtrl.js
+++ app/js/controllers/voteCtrl.js
@@ -3,36 +3,48 @@
 /* Controleur de la home page */
 
 angular.module('alveolus.voteCtrl', []).
-controller('VoteCtrl', function($scope,$routeParams,WebappService) {
-
-    $scope.isCollapsed = true;
-    $scope.toogleButtonContent = "Afficher la description";
-    loadList();
-    /**
-    * Initialiase les variables au chargement de la page (une seule fois)
-    **/
+controller('VoteCtrl', function($scope,$routeParams,WebappService,UserService) {
+    function init(){
+        /**
+        * Teste si l'utilisateur est loggué et affiche la modale de connexion ou
+        * ou la liste des alvéoles non validées en fonction
+        **/
+        if(!$scope.isLogged)
+            $scope.openModalLogin();
+        else
+            loadList();
+    }
     function loadList(){
-        // On commence par charger les catégories
-        WebappService.query(function(data){
-            //console.log(data);
+        /**
+        * Charge la liste des alvéoles non validées
+        **/
+        WebappService.getUnvalidated(function(data){
             $scope.alveoles = data;
-            data.sort(function(a, b){
+            $scope.alveoles.sort(function(a, b){
                 // Classe le tableau dans l'ordre décroissant en fonction de "created_at"
                 return(new Date(b['created_at'])-new Date(a['created_at']));
             });
-            for(var a in data){
+            for(var alveole_id in $scope.alveoles){
                 //Récupère l'utilisateur qui a posté l'alvéole
-                data[a].user = data[a].user_id ? User.get({id: data[a].user_id}) : {'pseudo':'l\'équipe'};
+                // a modifier une fois qu'on aura l'objet User dans Webapp
+                $scope.alveoles[alveole_id].user = $scope.alveoles[alveole_id].user_id ?
+                                                    UserService.get({id: $scope.alveoles[alveole_id].user_id})
+                                                    : {'pseudo':'l\'équipe'};
             }
         });
     }
-
-    $scope.vote = function(webAppId,voteValue){
-        WebappService.vote({id: webAppId, value: voteValue}, function(data){
-            $scope.data = data;
-        });
-        
+    $scope.vote = function(webAppId, index, voteValue){
+        if($scope.isLogged)
+            WebappService.vote({id: webAppId, value: voteValue}, function(data){
+                $scope.alveoles[index] = data;
+            });
     };
+
+    $scope.isCollapsed = true;
+    $scope.toogleButtonContent = "Afficher la description";
+    $scope.$on('onLoggedSuccess', function(){
+        loadList();});
+    init();
 
 
 });

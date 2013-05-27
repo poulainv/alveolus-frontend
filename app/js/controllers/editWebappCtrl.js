b9e35ae5d69174cdf032cb794c7f36a4d059f7f4
Clément <deonclem@gmail.com>
2013-05-27 15:56:15 +0200

    beginning facebook sso auth + some html/css changes

--- app/js/controllers/editWebappCtrl.js
+++ app/js/controllers/editWebappCtrl.js
@@ -3,9 +3,10 @@
 /* Controleur de la home page */
 
 angular.module('alveolus.editWebappCtrl', []).
-controller('EditWebappCtrl', function($scope,$routeParams,$location,WebappService) {
+controller('EditWebappCtrl', function($scope,$routeParams,$location,WebappService,CategoryService) {
 
 	$scope.webapp = WebappService.get({id:$routeParams.webAppId});
+	$scope.categories=CategoryService.query();		
 
 	$scope.submit = function(webapp){
 		console.log('edit webapp :');
--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -4,9 +4,78 @@
 
 
 angular.module('alveolus.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+directive('facebook', function($http) {
+  return {
+    restrict: 'A',
+    scope: true,
+    controller: function($scope, $attrs) {
+      // Load the SDK Asynchronously
+      (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+      }(document));
+
+      function login() {
+        FB.login(function(response) {
+          if (response.authResponse) {
+            console.log('FB.login connected');
+            fetch();
+          } else {
+            console.log('FB.login cancelled');
+          }
+          }, { scope: 'email,read_stream' }
+        );
+      };
+
+      function fetch() {
+        $http.post('/facebook/fetch', $scope.auth
+           ).success(function(data) {
+            window.location.reload(true);
+            $scope.fetch_status = data.status;
+          }).error(function(data) {
+            console.log('error: '+data);
+            $scope.fetch_status = data.status;
+          });
+      }
+
+      $scope.fetch = function() {
+        if ($scope.login_status == 'connected') {
+          console.log('fetch');
+          fetch();
+        } else {
+          login();
+        }
+      };
+    },
+    link: function(scope, element, attrs, controller) {
+      // Additional JS functions here
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : attrs.facebook, // App ID
+          channelUrl : '//localhost:3000/channel.html', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+
+        // Additional init code here
+        FB.getLoginStatus(function(response) {
+          if (response.status === 'connected') {
+            // connected
+            scope.auth = response.authResponse;
+          } else if (response.status === 'not_authorized') {
+            // not_authorized
+          } else {
+            // not_logged_in
+          }
+          scope.login_status = response.status;
+          scope.$apply();
+        });
+      }; // end of fbAsyncInit
+    }
+  }
+});
 

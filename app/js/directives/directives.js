1db2556b8726382f64983ce9d5c074aeccf3004e
vpoulain <vincent.poulain2@gmail.com>
2013-06-14 01:29:48 +0200

    update fb id

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -50,7 +50,7 @@ directive('facebook', function($http,globals) {
       window.fbAsyncInit = function() {
       	FB.init({
           appId      : attrs.facebook, // App ID
-          channelUrl : '//localhost:3000/channel.html', // Channel File
+          channelUrl : '//alveolus.fr/channel.html', // Channel File
           status     : true, // check login status
           cookie     : true, // enable cookies to allow the server to access the session
           xfbml      : true  // parse XFBML
